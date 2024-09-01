e0d4a79c3aa1e6d3a5719ff475b1a4536dd13f8c
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-08-31 21:27:58 -0500

    Did backend of login system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ConvexAuthNextjsServerProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
+          </body>
+      </html>
+    </ConvexAuthNextjsServerProvider>
   );
 }
