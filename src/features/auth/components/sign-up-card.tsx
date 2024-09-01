641ce22d15a81b188646aaf8df2261ed5059a02d
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-08-31 22:24:24 -0500

    Completed login and logout logic and user profile button

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -18,6 +18,7 @@ interface SignUpCardProps {
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
     const { signIn } = useAuthActions();
 
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
@@ -33,16 +34,16 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
         }
 
         setPending(true);
-        signIn("password", {email, password, flow: "signUp"})
-        .catch(() => {
-            setError("Something went wrong.");
-        })
-        .finally(() => {
-            setPending(false);
-        })
+        signIn("password", { name, email, password, flow: "signUp" })
+            .catch(() => {
+                setError("Something went wrong.");
+            })
+            .finally(() => {
+                setPending(false);
+            })
 
     }
-    
+
     const onProviderSignUp = (value: "github" | "google") => {
         setPending(true);
         signIn(value)
@@ -50,7 +51,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
                 setPending(false);
             })
     }
-    
+
 
     return (
         <Card className="w-full h-full p-8">
@@ -59,72 +60,79 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
                     Sign up to continue
                 </CardTitle>
                 <CardDescription>
-                Use your email or another service to continue
+                    Use your email or another service to continue
                 </CardDescription>
             </CardHeader>
             {!!error && (
                 <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
-                    <TriangleAlert className="size-4"/ >
+                    <TriangleAlert className="size-4" />
                     <p>{error}</p>
                 </div>
             )}
             <CardContent className="space-y-5 px-0 pb-0">
                 <form onSubmit={onPasswordSignUp} className="space-y-2.5">
-                    <Input 
-                    disabled={pending}
-                    value={email}
-                    onChange={(e)=> setEmail(e.target.value)}
-                    placeholder="Email"
-                    type="email"
-                    required
+                    <Input
+                        disabled={pending}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Full Name"
+                        required
                     />
-                    <Input 
-                    disabled={pending}
-                    value={password}
-                    onChange={(e)=> setPassword(e.target.value)}
-                    placeholder="Password"
-                    type="password"
-                    required
+                    <Input
+                        disabled={pending}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                        type="email"
+                        required
                     />
-                    <Input 
-                    disabled={pending}
-                    value={confirmPassword}
-                    onChange={(e)=> setConfirmPassword(e.target.value)}
-                    placeholder="Confirm password"
-                    type="password"
-                    required
+                    <Input
+                        disabled={pending}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        type="password"
+                        required
+                    />
+                    <Input
+                        disabled={pending}
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder="Confirm password"
+                        type="password"
+                        required
                     />
                     <Button type="submit" className="w-full" size="lg" disabled={pending}>
                         Continue
                     </Button>
                 </form>
-                <Separator/>
+                <Separator />
                 <div className="flex flex-col gap-y-2.5">
                     <Button
-                    disabled={pending}
-                    onClick={()=>onProviderSignUp("google")}
-                    variant="outline"
-                    size="lg"
-                    className="w-full relative"
+                        disabled={pending}
+                        onClick={() => onProviderSignUp("google")}
+                        variant="outline"
+                        size="lg"
+                        className="w-full relative"
                     >
-                        <FcGoogle className="size-5 absolute top-3 left-2.5"/>
+                        <FcGoogle className="size-5 absolute top-3 left-2.5" />
                         Continue with Google
                     </Button>
                     <Button
-                    disabled={pending}
-                    onClick={()=>onProviderSignUp("github")}
-                    variant="outline"
-                    size="lg"
-                    className="w-full relative"
+                        disabled={pending}
+                        onClick={() => onProviderSignUp("github")}
+                        variant="outline"
+                        size="lg"
+                        className="w-full relative"
                     >
-                        <FaGithub className="size-5 absolute top-3 left-2.5"/>
+                        <FaGithub className="size-5 absolute top-3 left-2.5" />
                         Continue with Github
                     </Button>
                 </div>
                 <p className="text-xs text-muted-foreground">
                     Already have an account? <span onClick={() => setState("signIn")} className="text-sky-700 hover:underline cursor-pointer">Sign In</span>
                 </p>
-            </CardContent>        
-            </Card>
+            </CardContent>
+        </Card>
     );
 };
